9eddb71410d1f33089239557521460a4df1aab32
Thomas Billequin <billequin.thomas@gmail.com>
2022-04-23 18:41:55 +0200

    ajout de scrollable parotut

--- app/src/main/java/edu/polytech/gotoslim/MainActivity.java
+++ app/src/main/java/edu/polytech/gotoslim/MainActivity.java
@@ -3,6 +3,7 @@ package edu.polytech.gotoslim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.view.Window;
 import android.os.Bundle;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
